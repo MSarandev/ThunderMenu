e96b0ae3d7d5cc854b02826aa1972062a9ff188f
MSarandev <maxsarandev@gmail.com>
2017-05-31 13:58:41 +0100

    Initializing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!-- VERSION: 0.0.1 & UPDATE: 31/05/17 -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thunder Menu</title>
+</head>
+<body>
+
+<p>Hello World!</p>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +14,6 @@
  * Date: 31-May-17
  * Time: 1:56 PM
  */
+?>
+</body>
+</html>
